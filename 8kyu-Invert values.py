dcd98ac72c1cbc70e2acfb307f6a9cd3c6b65c3f
Sina Tavoosi <110530257+sinatavoosi97@users.noreply.github.com>
2022-11-02 00:33:11 +0330

    Add files via upload

---
+++ 8kyu-Invert values.py
@@ -0,0 +1,17 @@
+# DESCRIPTION:
+# Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+# invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+# invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+# invert([]) == []
+# You can assume that all values are integers. Do not mutate the input array/list.
+
+def invert(lst):
+
+    list1=[]
+    for item in lst:
+       list1.append(item*-1)
+    
+    return list1
+
+    
