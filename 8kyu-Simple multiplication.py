c42b725f672f375249586e78b31e29aa37c6ba5e
Sina Tavoosi <110530257+sinatavoosi97@users.noreply.github.com>
2022-11-02 20:39:58 +0330

    Add files via upload

---
+++ 8kyu-Simple multiplication.py
@@ -0,0 +1,16 @@
+# DESCRIPTION:
+# This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+# FUNDAMENTALS
+
+
+def simple_multiplication(number) :
+
+    if number%2==0:
+        sum=8*number
+    
+    else:
+        sum=9*number
+    
+    return sum
+
