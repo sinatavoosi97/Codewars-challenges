f0fc19d78d56e94b20886f57f5b76ed939be1b52
Sina Tavoosi <110530257+sinatavoosi97@users.noreply.github.com>
2022-11-02 20:51:09 +0330

    Add files via upload

---
+++ 8kyu-Sum of positive.py
@@ -0,0 +1,16 @@
+# DESCRIPTION:
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+# Note: if there is nothing to sum, the sum is default to 0.
+
+# ARRAYSFUNDAMENTALS
+
+def positive_sum(arr):
+    sum=0
+    for item in arr:
+        if item >0:
+            sum+=item
+
+    return sum
