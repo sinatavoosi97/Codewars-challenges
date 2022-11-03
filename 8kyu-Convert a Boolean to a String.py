820d2feb03f5018aae0f47e314fd444ae5b241a9
Sina Tavoosi <110530257+sinatavoosi97@users.noreply.github.com>
2022-11-03 11:59:56 +0330

    Add files via upload

---
+++ 8kyu-Convert a Boolean to a String.py
@@ -0,0 +1,12 @@
+
+# DESCRIPTION:
+# Implement a function which convert the given boolean value into its string representation.
+
+# Note: Only valid inputs will be given.
+
+# STRINGSFUNDAMENTALS
+
+def boolean_to_string(b):
+
+    return str(b)
+
