7def18cfe7bede7a5ab7f73002f41540ba0c30b0
Sina Tavoosi <110530257+sinatavoosi97@users.noreply.github.com>
2022-11-03 11:25:05 +0330

    Add files via upload

---
+++ 8kyu- Opposite number.py
@@ -0,0 +1,15 @@
+# DESCRIPTION:
+# Very simple, given an integer or a floating-point number, find its opposite.
+
+# Examples:
+
+# 1: -1
+# 14: -14
+# -34: 34
+# FUNDAMENTALS
+
+
+def opposite(number):
+    number*=-1
+
+    return number
