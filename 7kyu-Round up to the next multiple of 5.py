3b632be693bc2395c6a83fbd62e463306ee8ef9b
Sina Tavoosi <110530257+sinatavoosi97@users.noreply.github.com>
2022-11-04 17:20:40 +0330

    Add files via upload

---
+++ 7kyu-Round up to the next multiple of 5.py
@@ -0,0 +1,29 @@
+# DESCRIPTION:
+# Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+# Examples:
+
+# input:    output:
+# 0    ->   0
+# 2    ->   5
+# 3    ->   5
+# 12   ->   15
+# 21   ->   25
+# 30   ->   30
+# -2   ->   0
+# -5   ->   -5
+# etc.
+# Input may be any positive or negative integer (including 0).
+
+# You can assume that all inputs are valid integers.
+
+# FUNDAMENTALS
+
+import math
+
+def round_to_next5(n):
+
+    b=n/5
+
+    return math.ceil(b)*5
+
