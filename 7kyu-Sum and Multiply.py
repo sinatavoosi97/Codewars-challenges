0777eca814021034aafd2392e1aef0799cb30313
Sina Tavoosi <110530257+sinatavoosi97@users.noreply.github.com>
2022-11-04 13:58:29 +0330

    Add files via upload

---
+++ 7kyu-Sum and Multiply.py
@@ -0,0 +1,48 @@
+# DESCRIPTION:
+# Write a function that accepts two parameters (sum and multiply) and find two numbers [x, y], where x + y = sum and x * y = multiply.
+
+# Example:
+
+# sum = 12 and multiply = 32
+
+# In this case, x equals 4 and y equals 8.
+
+# x = 4
+
+# y = 8
+
+# Because
+
+# x + y = 4 + 8 = 12 = sum
+
+# x * y = 4 * 8 = 32 = multiply
+
+# The result should be [4, 8].
+
+# Note:
+
+# 0 <= x <= 1000
+
+# 0 <= y <= 1000
+
+# If there is no solution, your function should return null (or None in python).
+
+# You should return an array (list in python) containing the two values [x, y] and it should be sorted in ascending order.
+
+# One last thing: x and y are integers (no decimals).
+
+# ALGORITHMS
+
+def sum_and_multiply(sum, multiply):
+
+    list1=[]
+    for i in range (1000):
+        for j in range(1000):
+            if i+j==sum and i*j==multiply:
+                    x,y=i,j
+                    list1=[x,y]
+                    list1.sort
+                    a=list1
+
+                    return a
+
