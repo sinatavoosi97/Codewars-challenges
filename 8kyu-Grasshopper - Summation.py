02e23c2e600775465c34ec19375c69d361902e89
Sina Tavoosi <110530257+sinatavoosi97@users.noreply.github.com>
2022-11-04 13:53:18 +0330

    Add files via upload

---
+++ 8kyu-Grasshopper - Summation.py
@@ -0,0 +1,23 @@
+# DESCRIPTION:
+# Summation
+# Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+# For example:
+
+# summation(2) -> 3
+# 1 + 2
+
+# summation(8) -> 36
+# 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+# FUNDAMENTALS MATHEMATICS
+
+def summation(num):
+    sum=0
+    for i in range(num+1):
+        sum+=i
+    
+    return sum
+
+
+print(summation(8))
+
