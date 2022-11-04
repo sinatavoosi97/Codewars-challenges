f13b2c338ee82f027fe539288b161bd0a223f810
Sina Tavoosi <110530257+sinatavoosi97@users.noreply.github.com>
2022-11-04 13:38:51 +0330

    Add files via upload

---
+++ 8kyu-Are You Playing Banjo.py
@@ -0,0 +1,23 @@
+# DESCRIPTION:
+# Create a function which answers the question "Are you playing banjo?".
+# If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+# The function takes a name as its only argument, and returns one of the following strings:
+
+# name + " plays banjo" 
+# name + " does not play banjo"
+# Names given are always valid strings.
+
+# STRINGS FUNDAMENTALS
+
+def are_you_playing_banjo(name):
+
+
+
+    if name[0]=="R" or  name[0]=="r":
+                name +=" plays banjo" 
+    else:
+                name +=" does not play banjo"
+
+    return name
+
