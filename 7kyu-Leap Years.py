6a389ede3d279e69f43be047e35e7cbd4e16395b
Sina Tavoosi <110530257+sinatavoosi97@users.noreply.github.com>
2024-10-27 20:46:12 +0330

    Add files via upload

---
+++ 7kyu-Leap Years.py
@@ -0,0 +1,46 @@
+def is_leap_year(year):
+
+    if 1600 <= year <= 4000 :
+
+
+        if year%100==0:
+            if year%400==0:
+
+                return True
+            
+            else:
+                return False
+        
+        if year%4==0:
+
+            return True
+        
+        else:
+             return False
+        
+ 
+
+    
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+        
+
+
+        
